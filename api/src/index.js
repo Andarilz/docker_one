2cbbd22077cea7a9426ccbd77fc7aff288cb5965
Andarilz <isgrammar@gmail.com>
2023-09-16 15:21:38 +0400

    Install express to api server

---
+++ api/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res)=> [
+	res.send("API working right")
+])
+
+
+app.listen(3000, () => {
+	console.log("Server listening at port 3000")
+})
