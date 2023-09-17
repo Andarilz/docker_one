5dc416b86a4c44bd01b96ee34deef478d8fa8886
Andarilz <isgrammar@gmail.com>
2023-09-17 16:07:30 +0400

    Save table and posts to mongo

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,41 @@
 const express = require("express")
 const { connectDb } = require("./helpers/db")
 const { port, host, db } = require("./config")
+const { mongoose } = require("mongoose")
 
 const app = express()
+
+const postSchema = new mongoose.Schema({
+	name: String
+})
+
+const Post = mongoose.model("Post", postSchema)
 
 
 const startServer = () => {
 	app.listen(port, () => {
 		console.log("Server listening at port " + port + " on host " + host)
 		console.log("Our database " + db)
+
+		Post.find()
+    .then(posts => {
+        console.log(posts, "posts");
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+		const silence = new Post({name: "Silence"})
+
+		silence.save()
+    .then(saved_posts => {
+			console.log("we saved " , saved_posts)
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+
 	})
 }
 
