992d90cdaa118db0e72e932b8d919ea7cc6259df
Andarilz <isgrammar@gmail.com>
2023-09-17 14:31:51 +0400

    Connect to Mongo

--- api/src/index.js
+++ api/src/index.js
@@ -1,17 +1,25 @@
 const express = require("express")
+const { connectDb } = require("./helpers/db")
+const { port, host, db } = require("./config")
 
 const app = express()
 
-const port = process.env.PORT
 
-const host = process.env.HOST
+const startServer = () => {
+	app.listen(port, () => {
+		console.log("Server listening at port " + port + " on host " + host)
+		console.log("Our database " + db)
+	})
+}
 
 
-app.get("/", (req, res)=> [
-	res.send("API working right")
-])
+app.get("/", (req, res)=>{
+	res.send("API working right")}
+)
 
 
-app.listen(port, () => {
-	console.log("Server listening at port " + port + " on host " + host)
-})
+connectDb()
+	.on("error", console.log)
+	.on("disconnect", connectDb)
+	.once("open", startServer)
+
